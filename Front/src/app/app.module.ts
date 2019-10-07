bcd3a905149e2e21f13c30558ebf08f09008965f
Esteban Le Calvez <lecalvez.esteban@gmail.com>
2019-10-07 16:26:43 +0200

    Integration CKEditor
    Ajout du CKEditor sur la page d'accueil

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CKEditorModule } from 'ckeditor4-angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
+    CKEditorModule,
     AppRoutingModule
   ],
   providers: [],
