6558ad0eb08b8a7f87f8e788cca7226b74e84c60
Guillaume <onyldas@gmail.com>
2019-10-07 17:06:30 +0200

    Creation modeles

--- Back/revisorBackProject/fiche/models.py
+++ Back/revisorBackProject/fiche/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Auteur(models.Model):
+    nom = models.CharField(max_length=30)
+    prenom = models.CharField(max_length=30)
+    mail = models.CharField(max_length=50)
+    mot_de_passe = models.CharField(min_length=8)
+
+class Sheet(models.Model):
+    titre = models.CharField(max_length=30)
+    image = models.CharField(max_length=256)
+    categorie = models.CharField(max_length=30)
+    date_crea = models.DateField(max_length=10)
+    date_last_maj = models.DateField(max_length=10)
+    contenu = models.CharField()
+    auteur = models.ForeignKey(Auteur, on_delete=models.CASCADE)
